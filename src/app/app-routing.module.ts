ef2ae2e1e0f9998a816bd8189aa457335390f954
Hawa-Evakali <hawa.evakali@student.moringaschool.com>
2022-04-20 18:33:39 +0300

    registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';XMLDocument
+import { RegisterComponent } from './register/register.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'Register', component: RegisterComponent }, {path: 'Details', component:DetailsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
