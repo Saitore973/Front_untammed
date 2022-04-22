08847970acece773bcfe4032a0942376993d6c90
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-22 07:42:46 +0300

    application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
-  { path: 'Untammed', component: LandingPageComponent},
-  { path: 'Login', component: LoginComponent}
+  { path: 'untammed', component: LandingPageComponent},
+  { path: 'Login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'event', component: DetailsComponent},
 ];
 
 @NgModule({
